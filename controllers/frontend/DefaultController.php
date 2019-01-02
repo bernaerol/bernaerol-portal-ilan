cc48b1e3b2421cc8bfde9264d091502aae119de9
bernaerol <bernae1998@gmail.com>
2019-01-02 21:42:34 +0300

    initial commit

---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\portal-ilan\controllers\frontend;
+
+
+/**
+ * Default controller for the `portal-ilan` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
